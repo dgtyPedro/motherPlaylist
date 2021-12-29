434d4f093ee8f66daad2c7e63161de998b7edec0
dgtyPedro <pedrogcs0104@gmail.com>
2021-12-29 16:39:55 -0300

    Vendor Folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0e78bc2c33ca92d7f5fa772861196f54e0093eef',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0e78bc2c33ca92d7f5fa772861196f54e0093eef',
+            'dev_requirement' => false,
+        ),
+        'jwilsson/spotify-web-api-php' => array(
+            'pretty_version' => '5.0.2',
+            'version' => '5.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jwilsson/spotify-web-api-php',
+            'aliases' => array(),
+            'reference' => '0e78bc2c33ca92d7f5fa772861196f54e0093eef',
+            'dev_requirement' => false,
+        ),
+    ),
+);
